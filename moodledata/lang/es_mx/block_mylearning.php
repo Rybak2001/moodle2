19c9fee07be2e46598562a1fda46b83b8573e2ad
root <root@ip-172-31-45-243.us-east-2.compute.internal>
2023-11-17 00:14:16 +0000

    h

---
+++ moodledata/lang/es_mx/block_mylearning.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_mylearning', language 'es_mx', version '3.11'.
+ *
+ * @package     block_mylearning
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['mylearning:addinstance'] = 'Añadir nuevo bloque de "Mi aprendizaje"';
+$string['mylearning:myaddinstance'] = 'Añadir nuevo bloque de "Mi aprendizaje"';
+$string['pluginname'] = 'Mi aprendizaje';
+$string['privacy:metadata'] = 'El bloque de Mi aprendizaje no almacena ningún dato personal.';
+$string['settingsdescription'] = 'Seleccionar el valor inicial del campo"{$a}" en el bloque "Mi aprendizaje" para usuarios nuevos.';
---
+++ moodledata/lang/es_mx/format_topicsactivitycards.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'format_topicsactivitycards', language 'es_mx', version '3.11'.
+ *
+ * @package     format_topicsactivitycards
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['activitydescription'] = 'Anular descripción de actividad';
+$string['additionalcssclasses'] = 'Clases CSS adicionales';
+$string['cardfooter'] = 'Pie de página de tarjeta';
+$string['cardimage'] = 'Imagen de tarjeta';
+$string['cardimagedescription'] = 'Una imagen de 398px por 174px';
+$string['cleanandtruncatedescription'] = 'Limpiar y truncar descripción';
+$string['duration'] = 'Duración';
+$string['hidefromothers'] = 'Ocultar tópico';
+$string['invalidcss'] = 'Clases CSS inválidas';
+$string['overlaycardimage'] = 'Sobrepuesto imagen tarjeta';
+$string['overridesectionsummary'] = 'Anular resumen de sección';
+$string['pluginname'] = 'Formato de tarjetas de actividad para tópicos';
+$string['privacy:metadata'] = 'El plugin de Formato de tarjetas de actividad para tópicos no almacena ningún dato personal.';
+$string['renderwidth'] = 'Ancho (de 12)';
+$string['sectionheading'] = 'Diseño de sección';
+$string['sectionheading_header'] = 'Encabezado con actividades';
+$string['sectionheading_linkedcard'] = 'Tarjeta enlazada';
+$string['sectionlayout'] = 'Diseño de actividades de sección';
+$string['sectionlayout_cards'] = 'Tarjetas';
+$string['sectionlayout_list'] = 'Lista';
+$string['showfromothers'] = 'Mostrar tópico';
